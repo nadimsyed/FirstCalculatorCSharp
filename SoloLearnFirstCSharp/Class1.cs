3e9dad3daf382e432873ac66178e7db82d0d1939
nadimsyed <syed.m.nadim@gmail.com>
2018-04-13 04:54:46 -0400

    First commit of first ever calculator

---
+++ SoloLearnFirstCSharp/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoloLearnFirstCSharp
+{
+    class Calculator
+    {
+        public static void checkInput(string number)
+        {
+            var isNumeric = double.TryParse(number, out double n);
+            if (!isNumeric)
+            {
+                Console.WriteLine("This is not a number or decimal");
+                System.Environment.Exit(1);
+            }
+
+        }
+
+       
+        public static void calculate()
+        {
+            
+            Console.Write("First number = ");
+            string first_Number = Console.ReadLine();
+            checkInput(first_Number);
+            double firstNumber = Convert.ToDouble(first_Number);
+
+            Console.Write("Enter operator: ");
+            string arth  = Console.ReadLine();
+
+            Console.Write("Second number = ");
+            string second_Number = Console.ReadLine();
+            checkInput(second_Number);
+            double secondNumber = Convert.ToDouble(second_Number);
+
+            double result = 0; 
+            if  (arth == "+")
+            {
+                result = firstNumber + secondNumber;              
+            } else if (arth == "-") {
+                result = firstNumber - secondNumber;               
+            } else if (arth == "*") {
+                result = firstNumber * secondNumber;
+            } else if (arth == "/") {
+                result = firstNumber / secondNumber;               
+            } else if  (arth == "%") {
+                result = firstNumber % secondNumber;
+            } else {
+                Console.WriteLine("Error 101: This is not an operator!!");
+                System.Environment.Exit(1);
+            }
+            Console.WriteLine("Answer is {0}", result);
+        }
+
+        
+
+    }
+}
---
+++ SoloLearnFirstCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SoloLearnFirstCSharp;
+
+namespace SoloLearnFirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.calculate();
+            /*
+            static void SayHi()
+            {
+                Console.WriteLine("Hello");
+            }
+            static void Main(string[] args)
+            {
+                SayHi();
+            }*/
+            return;
+        }
+    }
+}
